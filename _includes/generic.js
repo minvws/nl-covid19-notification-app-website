f12615b4f80ea90cbdcbb05e9a351ed6ec9d880b
Melle Wynia <hi@mellewynia.nl>
2020-07-07 07:58:00 +0200

    #60 #49: Impr. accessibility & Design v0.7

--- _includes/generic.js
+++ _includes/generic.js
@@ -1,6 +1,6 @@
 (function () {
     var htmlEl = document.querySelector('html');
-    htmlEl.setAttribute('class', htmlEl.getAttribute('class') + ' js-enabled');
+    htmlEl.setAttribute('class', (htmlEl.getAttribute('class') || '') + ' js-enabled');
 
     if (window.navigator.userAgent.indexOf('MSIE') >= 0 || !!navigator.userAgent.match(/Trident.*rv\:11\./)) {
         htmlEl.setAttribute('class', htmlEl.getAttribute('class') + ' browser-ie-11');
