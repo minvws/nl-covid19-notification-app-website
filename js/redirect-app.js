d5f8f1d73f8b488a1467522285389da795a0c28e
Hidde de Vries <hidde@hiddedevries.nl>
2020-08-13 16:23:55 +0200

    Fix if/else statement

--- js/redirect-app.js
+++ js/redirect-app.js
@@ -2,12 +2,14 @@
 // - Add dynamic URL's language?
 
 (function () {
-  if(navigator.userAgent.toLowerCase().indexOf("android") > -1){
-    window.location.href = 'https://play.google.com/store/apps/details?id=nl.rijksoverheid.en';
+  var redirectURL = '/';
+
+  if (navigator.userAgent.toLowerCase().indexOf('android') > -1) {
+    redirectURL = 'https://play.google.com/store/apps/details?id=nl.rijksoverheid.en';
   }
-  if(navigator.userAgent.toLowerCase().indexOf("iphone") > -1){
-    window.location.href = 'https://apps.apple.com/nl/app/id1517652429';
+  if (navigator.userAgent.toLowerCase().indexOf('iphone') > -1) {
+    redirectURL = 'https://apps.apple.com/nl/app/id1517652429';
   }
 
-  window.location.href = "/";
+  window.location = redirectURL;
 })();
