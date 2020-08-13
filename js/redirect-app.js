1ea3185d4a250055c8fc5909f1342b592be2b837
mikecodo <mike@smoothworks.nl>
2020-08-13 11:28:40 +0200

    added play store url to js/redirect-app.js

--- js/redirect-app.js
+++ js/redirect-app.js
@@ -1,10 +1,10 @@
 // TODO: 
 // - App store links plaatsen
-// - Add dynamic URL's language ?
+// - Add dynamic URL's language?
 
 (function () {
   if(navigator.userAgent.toLowerCase().indexOf("android") > -1){
-    window.location.href = 'https://play.google.com/store/apps/';
+    window.location.href = 'https://play.google.com/store/apps/details?id=nl.rijksoverheid.en';
   }
   if(navigator.userAgent.toLowerCase().indexOf("iphone") > -1){
     window.location.href = 'https://apps.apple.com/';
