5392ab4b3f89f2fa6135083eb4f641f69900ec07
Matijs Brinkhuis <matijs@gmail.com>
2020-08-14 12:43:23 +0200

    Improve accessibility of the language picker
    Add a button that toggles its 'aria-expanded' attribute between 'true'
    and 'false' which shows or hides the available languages accordingly.
    
    When the language-picker is open a keydown handler is bound to the
    document that will close the language-picker when Escape is pressed. The
    same goes for a click handler that watches for clicks outside the
    language-picker itself.

---
+++ js/language-picker.js
@@ -0,0 +1,43 @@
+(function() {
+	var languagePicker = document.querySelector('.language-picker');
+	var button = document.querySelector('[aria-controls="language-picker__dropdown"]');
+	var firstInteractive = document.querySelector('.language-picker__link');
+	var ariaExpanded;
+
+	if (!languagePicker) return;
+
+	ariaExpanded = button.getAttribute('aria-expanded') === 'true';
+
+	function toggleLanguagePicker() {
+		ariaExpanded = !ariaExpanded;
+		button.setAttribute('aria-expanded', ariaExpanded);
+
+		if (ariaExpanded) {
+			document.documentElement
+				.addEventListener('keydown', escapeKeydownHandler, true);
+			document.documentElement
+				.addEventListener('click', outsideClickHandler, true);
+			firstInteractive.focus();
+		} else {
+			document.documentElement
+				.removeEventListener('keydown', escapeKeydownHandler, true);
+			document.documentElement
+				.removeEventListener('click', outsideClickHandler, true);
+		}
+	}
+
+	function escapeKeydownHandler(event) {
+		if (event.key === 'Escape' || event.key === 'Esc') {
+			toggleLanguagePicker();
+			button.focus();
+		}
+	}
+
+	function outsideClickHandler(event) {
+		var target = event.target;
+		if (languagePicker.contains(target)) return;
+		toggleLanguagePicker();
+	}
+
+	button.addEventListener('click', toggleLanguagePicker);
+}());
