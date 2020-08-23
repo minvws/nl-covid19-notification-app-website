6b1e40e663494d69627ec0b7fd3fc307369467c6
Anneke Sinnema <mail@annekesinnema.nl>
2020-08-23 15:54:31 +0200

    Use buttons, not anchors, for navigating in slider

--- js/info-slider.js
+++ js/info-slider.js
@@ -9,7 +9,7 @@
             el: '.swiper-pagination',
             clickable: true,
             renderBullet: function (index, className) {
-                return '<a class="' + className + '" href="#swiper-slide-' + index + '"><span class="screen-reader-text">Ga naar slide ' + (index + 1) + '</span></a>';
+                return '<button class="' + className + '"><span class="screen-reader-text">Ga naar slide ' + (index + 1) + '</span></button>';
                 // TODO #91: render vanuit liquid en geef juist taal keys
             }
         },
