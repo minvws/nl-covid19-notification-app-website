9ddf40dac52045af44999cf7cacb6a99f340c486
Mike Leander <mike@smoothworks.nl>
2020-09-03 21:01:40 +0200

    Added animations for story titles & text, improved js

--- js/story.js
+++ js/story.js
@@ -1,16 +1,31 @@
 (function() {
-  window.addEventListener('scroll', function (event) {
-    document.querySelectorAll('.stories__story').forEach(function(story) {
-        var child = story.querySelector('.stories__story-image');
-        if (isInViewport(story)) {
-          child.classList.add("fade-in");
-        }
-    });
-  }, false);
-
+  window.addEventListener('scroll', animateStory);
 })();
 
-var isInViewport = function (elem) {
+function animateStory() {
+  var stories = document.querySelectorAll('.stories__story');
+  var storiesShown = document.querySelectorAll('.stories-image__animate');
+  if(storiesShown.length >= stories.length) {
+    window.removeEventListener('scroll', animateStory);
+    return;
+  }
+
+  stories.forEach(function(story) {
+    var image = story.querySelector('.stories__story-image');
+    var titleSmall = story.querySelector('.stories__story-title-small');
+    var title = story.querySelector('.stories__story-title');
+    var text = story.querySelector('.stories__story-text');
+    if (isInViewport(story)) {
+      image.classList.add('stories-image__animate');
+      titleSmall.classList.add('stories-title-small__animate');
+      title.classList.add('stories-title__animate');
+      text.classList.add('stories-text__animate');
+    }
+
+  });
+}
+
+function isInViewport(elem) {
     var bounding = elem.getBoundingClientRect();
     return (
         bounding.top >= 0 &&
