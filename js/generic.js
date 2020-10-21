bf9cdff11e8144c9c246deb8aa7f81639864a6da
Melle Wynia <hi@mellewynia.nl>
2020-10-21 16:17:20 +0200

    Git #403: Add Huawei AppGallery button

--- js/generic.js
+++ js/generic.js
@@ -3,12 +3,25 @@
     htmlEl.setAttribute('class', (htmlEl.getAttribute('class') || '') + ' js-enabled');
 
     if (window.navigator.userAgent.indexOf('MSIE') >= 0 || !!navigator.userAgent.match(/Trident.*rv\:11\./)) {
-       htmlEl.setAttribute('class', htmlEl.getAttribute('class') + ' browser-ie-11');
+        htmlEl.setAttribute('class', htmlEl.getAttribute('class') + ' browser-ie-11');
     }
-    if (navigator.userAgent.toLowerCase().indexOf('android') > -1) {
+
+    var userAgent = navigator.userAgent.toLowerCase();
+
+    if (userAgent.indexOf('android') > -1) {
       htmlEl.setAttribute('class', htmlEl.getAttribute('class') + ' android');
     }
-    if (navigator.userAgent.toLowerCase().indexOf('iphone') > -1) {
+    if (userAgent.indexOf('android') > -1) {
+      // Some Huawei devices do not support Google Play Store
+      if (! (userAgent.indexOf('hmscore') > -1)) {
+        htmlEl.setAttribute('class', htmlEl.getAttribute('class') + ' google-play-store');
+      }
+
+      if (userAgent.indexOf('hmscore') > -1) {
+        htmlEl.setAttribute('class', htmlEl.getAttribute('class') + ' huawei-app-gallery');
+      }
+    }
+    if (userAgent.indexOf('iphone') > -1) {
       htmlEl.setAttribute('class', htmlEl.getAttribute('class') + ' iOS');
     }
 
