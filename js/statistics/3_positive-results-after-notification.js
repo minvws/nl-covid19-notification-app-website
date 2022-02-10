1366094a08134233b4c3cfc43b0d582cb8cdfa25
Anneke Sinnema <mail@annekesinnema.nl>
2022-02-10 21:45:57 +0100

    Added first generated graphs
    Signed-off-by: Anneke Sinnema <mail@annekesinnema.nl>

--- js/statistics/3_positive-results-after-notification.js
+++ js/statistics/3_positive-results-after-notification.js
@@ -1,4 +1,103 @@
-positive_results_after_notification = [
-    {"weeknr": "12", "downloadnr": 0.000237, "date": "2021-03-28"},
-    {"weeknr": "13", "downloadnr": 0.000331, "date": "2021-04-04"},
+// ggd_positive_test_authorisations
+
+const positive_results_after_notification_raw = [
+  {
+    date: "2020-08-31",
+    ggd_daily: 5,
+    ggd_cumulative: 27
+  },
+  {
+    date: "2020-09-30",
+    ggd_daily: 30,
+    ggd_cumulative: 417
+  },
+  {
+    date: "2020-10-31",
+    ggd_daily: 1386,
+    ggd_cumulative: 22747
+  },
+  {
+    date: "2020-11-30",
+    ggd_daily: 569,
+    ggd_cumulative: 45795
+  },
+  {
+    date: "2020-12-31",
+    ggd_daily: 1164,
+    ggd_cumulative: 84140
+  },
+  {
+    date: "2021-01-31",
+    ggd_daily: 390,
+    ggd_cumulative: 106453
+  },
+  {
+    date: "2021-02-28",
+    ggd_daily: 518,
+    ggd_cumulative: 118318
+  },
+  {
+    date: "2021-03-31",
+    ggd_daily: 768,
+    ggd_cumulative: 138108
+  },
+  {
+    date: "2021-04-30",
+    ggd_daily: 0,
+    ggd_cumulative: 160019
+  },
+  {
+    date: "2021-05-31",
+    ggd_daily: 172,
+    ggd_cumulative: 176022
+  },
+  {
+    date: "2021-06-30",
+    ggd_daily: 49,
+    ggd_cumulative: 178726
+  },
+  {
+    date: "2021-07-31",
+    ggd_daily: 162,
+    ggd_cumulative: 194596
+  },
+  {
+    date: "2021-08-31",
+    ggd_daily: 108,
+    ggd_cumulative: 198044
+  },
+  {
+    date: "2021-09-30",
+    ggd_daily: 65,
+    ggd_cumulative: 200450
+  },
+  {
+    date: "2021-10-31",
+    ggd_daily: 553,
+    ggd_cumulative: 208408
+  },
+  {
+    date: "2021-11-30",
+    ggd_daily: 820,
+    ggd_cumulative: 235765
+  },
+  {
+    date: "2021-12-31",
+    ggd_daily: 815,
+    ggd_cumulative: 257602
+  },
+  {
+    date: "2022-01-31",
+    ggd_daily: 2476,
+    ggd_cumulative: 305182
+  },
+  {
+    date: "2022-02-07",
+    ggd_daily: 2154,
+    ggd_cumulative: 322773,
+  }
 ]
+
+const positive_results_after_notification = positive_results_after_notification_raw.map(({date, ...rest}) => {
+  return {date: new Date(date), ...rest};
+});
--- js/statistics/4_approached-through-bco.js
+++ js/statistics/4_approached-through-bco.js
@@ -1,4 +1,101 @@
-approached_through_bco = [
-    {"weeknr": "12", "downloadnr": 0.000237, "date": "2021-03-28"},
-    {"weeknr": "13", "downloadnr": 0.000331, "date": "2021-04-04"},
+const approached_through_bco_raw = [
+  {
+    date: "2020-08-31",
+    tested_positive: 15,
+    approached: 27
+  },
+  {
+    date: "2020-09-30",
+    tested_positive: 230,
+    approached: 417
+  },
+  {
+    date: "2020-10-31",
+    tested_positive: 10386,
+    approached: 22747
+  },
+  {
+    date: "2020-11-30",
+    tested_positive: 20569,
+    approached: 45795
+  },
+  {
+    date: "2020-12-31",
+    tested_positive: 21164,
+    approached: 84140
+  },
+  {
+    date: "2021-01-31",
+    tested_positive: 20390,
+    approached: 106453
+  },
+  {
+    date: "2021-02-28",
+    tested_positive: 20518,
+    approached: 118318
+  },
+  {
+    date: "2021-03-31",
+    tested_positive: 20768,
+    approached: 138108
+  },
+  {
+    date: "2021-04-30",
+    tested_positive: 60768,
+    approached: 160019
+  },
+  {
+    date: "2021-05-31",
+    tested_positive: 20172,
+    approached: 176022
+  },
+  {
+    date: "2021-06-30",
+    tested_positive: 20349,
+    approached: 178726
+  },
+  {
+    date: "2021-07-31",
+    tested_positive: 202162,
+    approached: 194596
+  },
+  {
+    date: "2021-08-31",
+    tested_positive: 20108,
+    approached: 198044
+  },
+  {
+    date: "2021-09-30",
+    tested_positive: 23065,
+    approached: 200450
+  },
+  {
+    date: "2021-10-31",
+    tested_positive: 172553,
+    approached: 208408
+  },
+  {
+    date: "2021-11-30",
+    tested_positive: 122820,
+    approached: 235765
+  },
+  {
+    date: "2021-12-31",
+    tested_positive: 153815,
+    approached: 257602
+  },
+  {
+    date: "2022-01-31",
+    tested_positive: 102476,
+    approached: 305182
+  },
+  {
+    date: "2022-02-07",
+    tested_positive: 202154,
+    approached: 322773,
+  }
 ]
+
+const approached_through_bco = approached_through_bco_raw.map(({date, ...rest}) => {
+  return {date: new Date(date), ...rest};
+});
