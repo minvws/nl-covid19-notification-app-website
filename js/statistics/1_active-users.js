0ebab6ade6868d982281f7fd0c434eedcf3fbb0c
Anneke Sinnema <mail@annekesinnema.nl>
2022-02-14 15:31:26 +0100

    Added page/graphics with dummy data
    Signed-off-by: Anneke Sinnema <mail@annekesinnema.nl>

--- js/statistics/1_active-users.js
+++ js/statistics/1_active-users.js
@@ -1,5 +1,88 @@
-active_users = [
-    {"weeknr": "12", "downloadnr": 0.004947, "date": "2021-03-28"},
-    {"weeknr": "13", "downloadnr": 0.005498, "date": "2021-04-04"},
+const active_users_raw = [
+  {
+    date: "2020-08-31",
+    active_users: 27
+  },
+  {
+    date: "2020-09-30",
+    active_users: 417
+  },
+  {
+    date: "2020-10-31",
+    active_users: 22747
+  },
+  {
+    date: "2020-11-30",
+    active_users: 45795
+  },
+  {
+    date: "2020-12-31",
+    tested_positive: 21164,
+    active_users: 84140
+  },
+  {
+    date: "2021-01-31",
+    tested_positive: 20390,
+    active_users: 106453
+  },
+  {
+    date: "2021-02-28",
+    tested_positive: 20518,
+    active_users: 118318
+  },
+  {
+    date: "2021-03-31",
+    active_users: 138108
+  },
+  {
+    date: "2021-04-30",
+    tested_positive: 60768,
+    active_users: 160019
+  },
+  {
+    date: "2021-05-31",
+    active_users: 176022
+  },
+  {
+    date: "2021-06-30",
+    tested_positive: 20349,
+    active_users: 182726
+  },
+  {
+    date: "2021-07-31",
+    tested_positive: 12162,
+    active_users: 194596
+  },
+  {
+    date: "2021-08-31",
+    active_users: 198044
+  },
+  {
+    date: "2021-09-30",
+    active_users: 200450
+  },
+  {
+    date: "2021-10-31",
+    active_users: 208408
+  },
+  {
+    date: "2021-11-30",
+    active_users: 235765
+  },
+  {
+    date: "2021-12-31",
+    active_users: 257602
+  },
+  {
+    date: "2022-01-31",
+    active_users: 305182
+  },
+  {
+    date: "2022-02-07",
+    active_users: 322773,
+  }
 ]
 
+const active_users = active_users_raw.map(({date, ...rest}) => {
+  return {date: new Date(date), ...rest};
+});
--- js/statistics/2_warning-notifications.js
+++ js/statistics/2_warning-notifications.js
@@ -1,4 +1,82 @@
-warning_notifications = [
-    {"weeknr": "12", "downloadnr": 0.000237, "date": "2021-03-28"},
-    {"weeknr": "13", "downloadnr": 0.000331, "date": "2021-04-04"},
+const warning_notifications_raw = [
+  {
+    date: "2020-08-31",
+    notifications: 27
+  },
+  {
+    date: "2020-09-30",
+    notifications: 417
+  },
+  {
+    date: "2020-10-31",
+    notifications: 22747
+  },
+  {
+    date: "2020-11-30",
+    notifications: 45795
+  },
+  {
+    date: "2020-12-31",
+    notifications: 84140
+  },
+  {
+    date: "2021-01-31",
+    notifications: 106453
+  },
+  {
+    date: "2021-02-28",
+    notifications: 118318
+  },
+  {
+    date: "2021-03-31",
+    notifications: 138108
+  },
+  {
+    date: "2021-04-30",
+    notifications: 160019
+  },
+  {
+    date: "2021-05-31",
+    notifications: 176022
+  },
+  {
+    date: "2021-06-30",
+    notifications: 178726
+  },
+  {
+    date: "2021-07-31",
+    notifications: 194596
+  },
+  {
+    date: "2021-08-31",
+    notifications: 198044
+  },
+  {
+    date: "2021-09-30",
+    notifications: 200450
+  },
+  {
+    date: "2021-10-31",
+    notifications: 208408
+  },
+  {
+    date: "2021-11-30",
+    notifications: 235765
+  },
+  {
+    date: "2021-12-31",
+    notifications: 257602
+  },
+  {
+    date: "2022-01-31",
+    notifications: 305182
+  },
+  {
+    date: "2022-02-07",
+    notifications: 322773,
+  }
 ]
+
+const warning_notifications = warning_notifications_raw.map(({date, ...rest}) => {
+  return {date: new Date(date), ...rest};
+});
