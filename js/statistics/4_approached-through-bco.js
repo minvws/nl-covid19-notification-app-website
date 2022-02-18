493fb44acc307d3231a92b93f8e83c6479c499e5
Anneke Sinnema <mail@annekesinnema.nl>
2022-02-18 01:16:51 +0100

    Renamed dummy stats keys to make usage easier
    Signed-off-by: Anneke Sinnema <mail@annekesinnema.nl>

--- js/statistics/4_approached-through-bco.js
+++ js/statistics/4_approached-through-bco.js
@@ -1,98 +1,98 @@
 const approached_through_bco_raw = [
   {
     date: "2020-08-31",
-    complaints: 15,
-    approached: 27
+    tested_positive: 15,
+    tested_total: 27
   },
   {
     date: "2020-09-30",
-    complaints: 230,
-    approached: 417
+    tested_positive: 230,
+    tested_total: 417
   },
   {
     date: "2020-10-31",
-    complaints: 10386,
-    approached: 22747
+    tested_positive: 10386,
+    tested_total: 22747
   },
   {
     date: "2020-11-30",
-    complaints: 20569,
-    approached: 45795
+    tested_positive: 20569,
+    tested_total: 45795
   },
   {
     date: "2020-12-31",
-    complaints: 21164,
-    approached: 84140
+    tested_positive: 21164,
+    tested_total: 84140
   },
   {
     date: "2021-01-31",
-    complaints: 20390,
-    approached: 106453
+    tested_positive: 20390,
+    tested_total: 106453
   },
   {
     date: "2021-02-28",
-    complaints: 20518,
-    approached: 118318
+    tested_positive: 20518,
+    tested_total: 118318
   },
   {
     date: "2021-03-31",
-    complaints: 20768,
-    approached: 138108
+    tested_positive: 20768,
+    tested_total: 138108
   },
   {
     date: "2021-04-30",
-    complaints: 60768,
-    approached: 160019
+    tested_positive: 60768,
+    tested_total: 160019
   },
   {
     date: "2021-05-31",
-    complaints: 20172,
-    approached: 176022
+    tested_positive: 20172,
+    tested_total: 176022
   },
   {
     date: "2021-06-30",
-    complaints: 20349,
-    approached: 182726
+    tested_positive: 20349,
+    tested_total: 182726
   },
   {
     date: "2021-07-31",
-    complaints: 12162,
-    approached: 194596
+    tested_positive: 12162,
+    tested_total: 194596
   },
   {
     date: "2021-08-31",
-    complaints: 20108,
-    approached: 198044
+    tested_positive: 20108,
+    tested_total: 198044
   },
   {
     date: "2021-09-30",
-    complaints: 23065,
-    approached: 200450
+    tested_positive: 23065,
+    tested_total: 200450
   },
   {
     date: "2021-10-31",
-    complaints: 172553,
-    approached: 208408
+    tested_positive: 172553,
+    tested_total: 208408
   },
   {
     date: "2021-11-30",
-    complaints: 122820,
-    approached: 235765
+    tested_positive: 122820,
+    tested_total: 235765
   },
   {
     date: "2021-12-31",
-    complaints: 153815,
-    approached: 257602
+    tested_positive: 153815,
+    tested_total: 257602
   },
   {
     date: "2022-01-31",
-    complaints: 102476,
-    approached: 305182
+    tested_positive: 102476,
+    tested_total: 305182
   },
   {
     date: "2022-02-07",
-    complaints: 202154,
-    approached: 322773,
+    tested_positive: 202154,
+    tested_total: 322773,
   }
 ]
 
